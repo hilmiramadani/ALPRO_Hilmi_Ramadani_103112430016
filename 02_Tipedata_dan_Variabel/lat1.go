38c40cb8f6f24563fe69595b6d66b087aa6d7955
hilmi <123058261+hilmiramadani@users.noreply.github.com>
2024-10-02 10:33:09 +0700

    Add files via upload

---
+++ 02_Tipedata_dan_Variabel/lat1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		satu, dua, tiga string
+		temp            string
+	)
+	fmt.Print("Masukan input string 1: ")
+	fmt.Scanln(&satu)
+	fmt.Print("Masukan input string 2: ")
+	fmt.Scanln(&dua)
+	fmt.Print("Masukan input string 3: ")
+	fmt.Scanln(&tiga)
+	fmt.Println("Output awal = " + satu + " " + dua + " " + tiga)
+	temp = satu
+	satu = dua
+	dua = tiga
+	tiga = temp
+	fmt.Println("Output akhir = " + satu + " " + dua + " " + tiga)
+}
+
+//Program tsb adalah menggabungkan inputan tetapi hasilnya di acak atau tidak urut
